75e484d2251111a99be0a2f8446c25ce0c48e169
Sanket Sayal <snktsayal@gmail.com>
2025-12-27 15:20:22 +0530

    Setting up backend server

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { config } from "./config/env";
+import routes from "./router";
+
+const app = express();
+const PORT = config.port;
+/* ----------- Middleware ----------- */
+app.use(cors);
+app.use(express.json());
+app.use(morgan("dev"));
+
+/* ----------- Routes ----------- */
+app.use("/api", routes)
+
+/* ----------- Health Check ----------- */
+app.get("/health", (_req, res) => {
+  res.status(200).json({
+    status: "ok",
+    service: "Payroll Anomaly Detector",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
